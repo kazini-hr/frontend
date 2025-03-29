9aa78d87a67b0f87f4f6482190f650385510a5d2
abracodeabra <akakiplimo@gmail.com>
2025-03-29 15:23:43 +0300

    student page

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import { role } from '../lib/data';
+import { role } from '@/app/lib/data';
 
 const menuItems = [
   {
