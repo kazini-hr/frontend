b7dd81cb0e468f9dfeb093372e62a4332d8a19e1
abracodeabra <akakiplimo@gmail.com>
2025-03-29 13:50:59 +0300

    add mock data and implement role-based menu

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import { role } from '../lib/data';
 
 const menuItems = [
   {
@@ -125,16 +126,20 @@ const Menu = () => {
           <span className="hidden lg:block text-gray-400 font-light my-4">
             {item.title}
           </span>
-          {item.items.map((subItem) => (
-            <Link
-              href={subItem.href}
-              key={subItem.label}
-              className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
-            >
-              <Image src={subItem.icon} alt="" width={20} height={20} />
-              <span className="hidden lg:block">{subItem.label}</span>
-            </Link>
-          ))}
+          {item.items.map((subItem) => {
+            if (subItem.visible.includes(role)) {
+              return (
+                <Link
+                  href={subItem.href}
+                  key={subItem.label}
+                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
+                >
+                  <Image src={subItem.icon} alt="" width={20} height={20} />
+                  <span className="hidden lg:block">{subItem.label}</span>
+                </Link>
+              );
+            }
+          })}
         </div>
       ))}
     </div>
